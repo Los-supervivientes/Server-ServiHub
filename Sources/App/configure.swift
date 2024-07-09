603355ed24bbfc0c1ca5b1faa4a975d085769b8e
Boncroix <boncroix@gmail.com>
2024-07-09 20:22:02 +0200

    Crear variables de entorno y conectar base de datos local

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,12 +3,19 @@ import Fluent
 import FluentPostgresDriver
 import Vapor
 
-// configures your application
+// MARK: - Configure App
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-
-    // register routes
+    // MARK: Environment Variables
+    guard let jwtKey = Environment.get("JWT_KEY") else { fatalError("JWT Key not found")}
+    guard let _ = Environment.process.API_KEY else {fatalError("JWT Key not found")}
+    guard let dbURL = Environment.process.DATABASE_URL else {fatalError("DB URL not found")}
+    guard let _ = Environment.process.APP_BUNDLE_ID else {fatalError("APP Bundle ID not found")}
+    
+    // MARK: DB Conection
+    try app.databases.use(.postgres(url: dbURL), as: .psql)
+    
+            
+    // MARK: Register Router
     try routes(app)
 }
